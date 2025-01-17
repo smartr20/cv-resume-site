e7775f4312bc737a0ef96da3f1ce39b315fe63cb
smartr20 <smartr20@gmail.com>
2025-01-17 21:09:10 +0330

    new cv resume site created

---
+++ CVresume/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for CVresume project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CVresume.settings')
+
+application = get_wsgi_application()
