9943b32dbe5e7b5466c02b2d98c254a8338e8f66
smartr20 <smartr20@gmail.com>
2025-01-20 08:54:59 +0330

    ch1

--- website/views.py
+++ website/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def index_view(request):
-    return render(request,'website/index.html')
+    profile={'name': 'Reza', 'family_name': 'Moradi', 'phone': '098912-353-2287', 'email':'smartr20@gmail.com'}
+    return render(request,'website/index.html', profile)
